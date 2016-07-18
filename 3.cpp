962b7b724fc98cdc2280327c60b38c3e0301ec50
root <root@localhost.localdomain>
2016-07-18 22:11:48 +0800

    add 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,30 @@
+#include <string>
+using std::string;
+class Solution {
+    public:
+        int lengthOfLongestSubstring(string s) {
+            string sub;
+            int max = 0;
+            int length = 0;
+            for (auto i : s) {
+                int start = sub.find(i);
+                if (start != string::npos) {
+                    sub = sub.substr(start + 1);
+                    length = sub.size();
+                } else {
+                    sub += i;
+                    length++;
+                    if (length > max) {
+                        max = length;
+                    }
+                }
+            }
+            return max;
+        }
+};
+
+int main() {
+    Solution s;
+    int max = s.lengthOfLongestSubstring("bbbbb");
+    return 0;
+}
