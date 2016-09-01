4a250b869946a471154521a19992021ed9253bb6
707 <707472783@qq.com>
2016-09-01 12:08:37 +0800

    update 181

--- 181.sql
+++ 181.sql
@@ -1,1 +1,3 @@
-select e.Name as Employee from Employee as e, Employee as m where e.ManagerId = m.Id and e.Salary > m.Salary
+select e.Name as Employee
+from Employee as e, Employee as m
+where e.ManagerId = m.Id and e.Salary > m.Salary
